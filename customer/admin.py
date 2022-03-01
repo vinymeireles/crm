a05c61426bff10ef41a8340b99b25a5ef373870f
Viny <viniciusmeireles@gmail.com>
2022-03-01 20:22:03 -0300

    Projeto CRM Django WEB

---
+++ customer/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Customer
+
+# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+   list_display = ["id", "first_name", "last_name", "email"] 
+
